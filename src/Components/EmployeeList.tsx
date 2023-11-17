7441ca02188f7a9658d5db3aee148b3a4c681497
ruxsara <87611734+ruxsara@users.noreply.github.com>
2023-11-18 00:28:48 +0400

    Feat:created EmployeeModal.style.css

--- src/Components/EmployeeList.tsx
+++ src/Components/EmployeeList.tsx
@@ -1,52 +1,50 @@
 import { IEmployee } from "./Employee.type";
-import "./EmployeeList.style.css"
+import "./EmployeeList.style.css";
+import { EmployeeModal } from "./EmployeeModal";
 
-type Props={
-list: IEmployee[];
-onDeleteClickHnd:(data:IEmployee)=> void
-}
+type Props = {
+  list: IEmployee[];
+  onDeleteClickHnd: (data: IEmployee) => void;
+};
 
-
-export const EmployeeList = (props:Props) => {
-    const {list,onDeleteClickHnd}= props
-    const onDeleteBtnClick=(data:IEmployee)=>{}
-  return(
-  <div>
-    <article>
-      <h3 className="list-header">Employee List</h3>
-    </article>
-    <table>
-      <thead>
-        <tr>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Action</th>
-      </tr>
-      </thead>
-      <tbody>
-      {list.map((employee)=>{
-        return (
-           
-       <tr key={employee.id}>
-        <td>{`${employee.firstName} ${employee.lastName} `}</td>
-        <td>{employee.email}</td>
-        <td>
-            <div>
-                <input type="button" value="View" />
-                <input type="button" value="Edit" />
-                <input type="button" value="Delete" onClick={onDeleteClickHnd} />
-
-            </div>
-        </td>
-      </tr>
-      
-        )
-      })}</tbody>
-
-      
-     
-      
-    </table>
-  </div>
-  )
+export const EmployeeList = (props: Props) => {
+  const { list, onDeleteClickHnd } = props;
+  return (
+    <div>
+      <article>
+        <h3 className="list-header">Employee List</h3>
+      </article>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {list.map((employee) => {
+            return (
+              <tr key={employee.id}>
+                <td>{`${employee.firstName} ${employee.lastName} `}</td>
+                <td>{employee.email}</td>
+                <td>
+                  <div>
+                    <input type="button" value="View" />
+                    <input type="button" value="Edit" />
+                    <input
+                      type="button"
+                      value="Delete"
+                      onClick={()=>{onDeleteClickHnd(employee)}}
+                    />
+                  </div>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <EmployeeModal />
+    </div>
+  );
 };
--- src/Components/EmployeeModal.tsx
+++ src/Components/EmployeeModal.tsx
@@ -1,4 +1,10 @@
-export const EmployeeModal=()=>{
-    return <div> </div>
-
-}
+export const EmployeeModal = () => {
+  return (
+    <div id="myModal" className="modal">
+      <div className="modal-content">
+        <span className="close">&times;</span>
+        <p>Some text in the Modal..</p>
+      </div>
+    </div>
+  );
+};
