c044cc12a8dcd87e970304cbc936a6168a31814b
ruxsara <87611734+ruxsara@users.noreply.github.com>
2023-11-17 18:24:58 +0400

    Feat: created Home.style.css file in Components file

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
 
-function Home() {
-  return (
-    <div>
-      
-    </div>
-  )
+const Home=()=>{
+ return <>
+ <article className="article-header">
+    <header >
+        <h1> React Simple CRUD Application</h1>
+    </header>
+ </article>
+
+        </>
 }
-
 export default Home
