3833b511b903f0454f678089aab54d0a79ce4375
ruxsara <87611734+ruxsara@users.noreply.github.com>
2023-11-17 18:35:00 +0400

    Feat: created Employee.type.ts in Components folder

---
+++ src/Components/Employee.type.ts
@@ -0,0 +1,6 @@
+ export interface IEmployee{
+    id:string,
+    firstName:string,
+    lastName:string,
+    email:string
+}
--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,4 +1,4 @@
-
+import "./Home.style.css"
 const Home=()=>{
  return <>
  <article className="article-header">
@@ -6,6 +6,12 @@ const Home=()=>{
         <h1> React Simple CRUD Application</h1>
     </header>
  </article>
+
+ <section className="section-content">
+    <div>
+        this is Content part
+    </div>
+ </section>
 
         </>
 }
